82b26f58db7f21fe0d5d8f455b9a53c9cd775fe4
Leandro Ugioni <leandro_ugioni@hotmail.com>
2022-10-07 00:16:35 -0300

    [feat] add new tests

---
+++ test/serve_rest.spec.ts
@@ -0,0 +1,36 @@
+import pactum from 'pactum';
+import { SimpleReporter } from '../simple-reporter';
+
+describe('ServeRest API', () => {
+  let token = '';
+  const p = pactum;
+  const rep = SimpleReporter;
+  const baseUrl = 'https://serverest.dev';
+
+  beforeAll(() => p.reporter.add(rep));
+  afterAll(() => p.reporter.end());
+
+  describe('Login', () => {
+    it('POST', async () => {
+      token = await p
+        .spec()
+        .post(`${baseUrl}/login`)
+        .withJson({
+          email: 'a@b.com.br',
+          password: 'admin.1234'
+        })
+        .expectStatus(200)
+        .returns('authorization');
+    });
+  });
+
+  describe('Usuários', () => {
+    it('GET ALL', async () => {
+      await p
+        .spec()
+        .get(`${baseUrl}/usuarios`)
+        .withHeaders('Authorization', token)
+        .expectStatus(200);
+    });
+  });
+});
