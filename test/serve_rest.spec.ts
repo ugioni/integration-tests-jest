78e02ca564c51bb45c487115bf25ad0bf18d9a19
Leandro Ugioni <leandro_ugioni@hotmail.com>
2022-10-14 01:12:21 -0300

    [feat] add business flow to serve rest

--- test/serve_rest.spec.ts
+++ test/serve_rest.spec.ts
@@ -4,61 +4,110 @@ import { faker } from '@faker-js/faker';
 
 describe('ServeRest API', () => {
   let token = '';
-  let id = '';
+  let idUsuario = '';
+  let idProduto = '';
+  let emailUsuario = '';
   const p = pactum;
   const rep = SimpleReporter;
   const baseUrl = 'https://serverest.dev';
 
-  beforeAll(() => p.reporter.add(rep));
-  afterAll(() => p.reporter.end());
+  beforeAll(async () => {
+    p.reporter.add(rep);
 
-  describe('Login', () => {
-    it('POST', async () => {
-      token = await p
-        .spec()
-        .post(`${baseUrl}/login`)
-        .withJson({
-          email: 'fulano@qa.com',
-          password: 'teste'
-        })
-        .expectStatus(200)
-        .returns('authorization');
-    });
+    idUsuario = await p
+      .spec()
+      .post(`${baseUrl}/usuarios`)
+      .withRequestTimeout(60000)
+      .withHeaders('monitor', false)
+      .withJson({
+        nome: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: '123456789',
+        administrador: 'true'
+      })
+      .expectStatus(201)
+      .returns('_id');
+
+    emailUsuario = await p
+      .spec()
+      .get(`${baseUrl}/usuarios/${idUsuario}`)
+      .withRequestTimeout(60000)
+      .withHeaders('monitor', false)
+      .expectStatus(200)
+      .returns('email');
   });
 
-  describe('Usuários', () => {
-    it('GET ALL', async () => {
-      await p
-        .spec()
-        .get(`${baseUrl}/usuarios`)
-        .withHeaders('Authorization', token)
-        .expectStatus(200);
-    });
+  beforeEach(async () => {
+    token = await p
+      .spec()
+      .post(`${baseUrl}/login`)
+      .withRequestTimeout(60000)
+      .withHeaders('monitor', false)
+      .withJson({
+        email: `${emailUsuario}`,
+        password: '123456789'
+      })
+      .expectStatus(200)
+      .returns('authorization');
   });
 
-  describe('produtos', () => {
-    it('post', async () => {
-      id = await p
+  describe('Produtos', () => {
+    it('Cadastro um novo produto', async () => {
+      idProduto = await p
         .spec()
         .post(`${baseUrl}/produtos`)
+        .withRequestTimeout(60000)
         .withHeaders('Authorization', token)
+        .withHeaders('monitor', false)
         .withJson({
-          nome: faker.commerce.product(),
-          preco: 1000,
-          descricao: 'notebook',
-          quantidade: 1
+          nome: faker.commerce.productName(),
+          preco: 500,
+          descricao: faker.commerce.productDescription(),
+          quantidade: 10
         })
         .expectStatus(201)
-        .expectBodyContains('Cadastro realizado com sucesso')
         .returns('_id');
     });
-
-    it('GET produto', async () => {
+    it('Busca o novo produto cadastrado', async () => {
       await p
         .spec()
-        .get(`${baseUrl}/produtos/${id}`)
+        .get(`${baseUrl}/produtos/${idProduto}`)
+        .withRequestTimeout(60000)
         .withHeaders('Authorization', token)
+        .withHeaders('monitor', false)
         .expectStatus(200);
     });
   });
+
+  describe('Carrinhos', () => {
+    it('Adiciona um novo carrinho', async () => {
+      await p
+        .spec()
+        .post(`${baseUrl}/carrinhos`)
+        .withRequestTimeout(60000)
+        .withHeaders('Authorization', token)
+        .withHeaders('monitor', false)
+        .withJson({
+          produtos: [
+            {
+              idProduto: `${idProduto}`,
+              quantidade: 10
+            }
+          ]
+        })
+        .expectStatus(201)
+        .returns('_id');
+    });
+    it('Conclui a compra e exclui o carrinho', async () => {
+      await p
+        .spec()
+        .delete(`${baseUrl}/carrinhos/concluir-compra`)
+        .withRequestTimeout(60000)
+        .withHeaders('Authorization', token)
+        .withHeaders('monitor', false)
+        .expectStatus(200);
+    });
+  });
+
+  afterAll(() => p.reporter.end());
 });
